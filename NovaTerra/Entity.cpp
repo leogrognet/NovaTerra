a890f32acc235098d32790e1a73386245dcb7799
leogrognet <lgrognet@gaming.tech>
2025-03-13 17:40:53 +0100

    Valid

--- NovaTerra/Entity.cpp
+++ NovaTerra/Entity.cpp
@@ -1,14 +1,14 @@
 #include "Entity.h"
 
-void Entity::update(float deltaTime, const std::vector<Entity>& colliders)
+void Entity::update(float deltaTime, const std::vector<Entity*>& colliders)
 {
 
     vector<FloatRect> vec;
     for (auto& collider : colliders) {
-        if (&collider.m_shape == &m_shape) {
+        if (&collider->m_shape == &m_shape) {
             continue;
         }
-        vec.push_back(collider.m_shape.getGlobalBounds());
+        vec.push_back(collider->m_shape.getGlobalBounds());
     }
     m_rigidBody.update(deltaTime, vec, m_shape.getGlobalBounds());
 }
